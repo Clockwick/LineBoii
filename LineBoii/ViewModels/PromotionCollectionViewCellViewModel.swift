b2cebb809d7abc5ed071b523d088694295f56b2a
arthris <oclockth@gmail.com>
2021-07-31 01:21:47 +0700

    Order Food Tab (On Process ⚙️): Promotion Collection View 📱

---
+++ LineBoii/ViewModels/PromotionCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PromotionCollectionViewCellViewModel.swift
+//  LineBoii
+//
+//  Created by Paratthakorn Sribunyong on 31/7/2564 BE.
+//
+
+import Foundation
+
+struct PromotionCollectionViewCellViewModel {
+    let title: String
+    let imageURL: URL?
+}
---
+++ LineBoii/Views/_2x4CollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  _2x4CollectionViewCell.swift
+//  LineBoii
+//
+//  Created by Paratthakorn Sribunyong on 31/7/2564 BE.
+//
+
+import UIKit
+
+class _2x4CollectionViewCell: UICollectionViewCell {
+    static let identifier = "_2x4CollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+//    func configure(with viewModel: )
+    
+}
